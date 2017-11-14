05ca278e43793e4948d2ae478b3dce39932abebc
railgunxin <1307881527@qq.com>
2017-11-14 13:59:47 +0800

    添加左侧布局

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,35 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <leftNav></leftNav>
+    <div class="main">
+      <router-view/>
+    </div>
+    
   </div>
 </template>
 
 <script>
+import leftNav from '@/components/common/leftNav'
 export default {
-  name: 'app'
+  name: 'app',
+  components: {
+    leftNav
+  }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Comic Sans MS', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+.main {
+  float: left;
+  width: 95%;
+  background-color: lightcyan;
+  height: 100%;
+  overflow: hidden;
 }
 </style>
