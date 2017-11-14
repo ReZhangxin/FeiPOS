671582da202274234871c37916421ee5745a8643
railgunxin <1307881527@qq.com>
2017-11-14 09:49:33 +0800

    pos

---
+++ src/components/page/Pos.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="pos">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to FeiPOS'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
